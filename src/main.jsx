c246ce6ab9d4aee3b3acb71826adfb729615b6a9
Beesalam <abdulsalambabatunde305@gmail.com>
2026-07-02 00:43:29 +0100

    pwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import { registerSW } from "virtual:pwa-register";
+import { registerSW } from 'virtual:pwa-register'
 
-registerSW({ immediate: true });
+registerSW({
+  immediate: true,
+  onOfflineReady() {
+    console.log('PWA is ready for offline use.')
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -16,34 +16,40 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
-
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+        type: 'module',
+      },
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'android-chrome-192x192.png', 'android-chrome-512x512.png'],
       manifest: {
-        name: "Gatherly",
-        short_name: "Gatherly",
-        description: "Event Management Platform",
-        theme_color: "#ffffff",
-        background_color: "#971111",
-        display: "standalone",
-        start_url: "/",
-icons: [
-  {
-    src: "/android-chrome-192x192.png",
-    sizes: "192x192",
-    type: "image/png",
-  },
-  {
-    src: "/android-chrome-512x512.png",
-    sizes: "512x512",
-    type: "image/png",
-  },
-  {
-    src: "/android-chrome-512x512.png",
-    sizes: "512x512",
-    type: "image/png",
-    purpose: "any maskable",
-  },
-],
+        name: 'Gatherly',
+        short_name: 'Gatherly',
+        description: 'Event Management Platform',
+        theme_color: '#ffffff',
+        background_color: '#971111',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        display_override: ['standalone', 'browser'],
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
       },
     }),
   ],
